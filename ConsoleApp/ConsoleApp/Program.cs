1b2a2cd2d6d48b32e08d7c1e97df1889ac341301
sumit.vbaddi@gmail.com <sumit.vbaddi@gmail.com>
2022-04-14 22:40:33 +0530

    FizzBuzz with Unit Testing

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter an integer:");
+            int i;
+            int.TryParse(Console.ReadLine(),out i);
+            var res = DisplayFizzBuzz(i);
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+
+        public static string DisplayFizzBuzz(int i)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (i % 3 == 0 && i % 5 != 0)
+            {
+                return "Fizz";
+            }
+            else if (i % 3 != 0 && i % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                return i.ToString();
+            }
+        }
+    }
+}
